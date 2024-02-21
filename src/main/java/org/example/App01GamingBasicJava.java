ae8d2aa586fa38dc8b233b11e3d8a3211fe8d003
pushprajsinghjadoun <pushprajsinghjadoun@outlook.com>
2024-02-21 22:47:10 +0530

    Exploring Java Spring XML Configuration

--- src/main/java/org/example/App01GamingBasicJava.java
+++ src/main/java/org/example/App01GamingBasicJava.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import org.example.game.*;
-
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class App01GamingBasicJava {
---
+++ src/main/java/org/example/example/h1/XmlConfigurationContextLauncherApplication.java
@@ -0,0 +1,23 @@
+package org.example.example.h1;
+
+import org.example.game.GameRunner;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Arrays;
+
+public class XmlConfigurationContextLauncherApplication {
+
+    public static void main(String[] args) {
+
+        try (var context =
+                     new ClassPathXmlApplicationContext("contextConfiguration.xml")) {
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+            System.out.println(context.getBean("name"));
+            System.out.println(context.getBean("age"));
+            context.getBean(GameRunner.class).run();
+        }
+    }
+}
