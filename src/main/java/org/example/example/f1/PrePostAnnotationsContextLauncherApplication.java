05fc1c3f21defcf1db955e55b79a2db7ff91c620
pushprajsinghjadoun <pushprajsinghjadoun@outlook.com>
2024-02-21 22:17:00 +0530

    Added code for PostConstruct and Jakarta context & Dependency Injection

---
+++ src/main/java/org/example/example/f1/PrePostAnnotationsContextLauncherApplication.java
@@ -0,0 +1,50 @@
+package org.example.example.f1;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+class SomeClass {
+    private SomeDependency someDependency;
+    public SomeClass(SomeDependency someDependency) {
+        super();
+        this.someDependency = someDependency;
+        System.out.println("All dependencies are ready!");
+    }
+    @PostConstruct
+    public void initialize() {
+       someDependency.getReady();
+    }
+    // Generally used to clean up resources
+    @PreDestroy
+    public void destroy() {
+        System.out.println("SomeClass is being destroyed or Cleanup!");
+    }
+}
+
+@Component
+class SomeDependency {
+    public void getReady() {
+        System.out.println("Some logic using SomeDependency!");
+    }
+}
+
+@Configuration
+@ComponentScan
+public class PrePostAnnotationsContextLauncherApplication {
+
+    public static void main(String[] args) {
+
+        try (var context =
+                     new AnnotationConfigApplicationContext
+                             (PrePostAnnotationsContextLauncherApplication.class)) {
+            Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
+        }
+    }
+}
